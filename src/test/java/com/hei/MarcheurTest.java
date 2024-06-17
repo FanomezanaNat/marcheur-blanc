5f0d249d00594b24e454c97052ed025fcea84a89
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-17 15:14:48 +0300

    wip:test: add est_vraiment_dernier_localisation

--- src/test/java/com/hei/MarcheurTest.java
+++ src/test/java/com/hei/MarcheurTest.java
@@ -61,6 +61,37 @@ public class MarcheurTest {
     }
 
 
+    @Test
+    public void est_vraiment_dernier_localisation() {
+        var Bjarni = new Marcheur("Bjarni");
+
+        var Hei = new Lieu("HEI");
+        var Pullman = new Lieu("Pullman");
+        var Nexta = new Lieu("Nexta");
+        var Sekolintsika = new Lieu("Sekolintsika");
+        var Marais = new Lieu("Marais");
+        var Balancoire = new Lieu("Balancoire");
+        var Esti = new Lieu("Esti");
+        var Boulevard = new Lieu("Boulevard de l'Europe");
+        var Meteo = new Lieu("Meteo");
+
+
+        var Andriatsihoarana = new Rue("Rue Andriatsihoarana", Hei, Pullman);
+        var Ranaivo = new Rue("Rue Ranaivo", Pullman, Balancoire);
+        var maraisSekoly = new Rue("Marais Sekoly", Marais, Sekolintsika);
+        var sekolyHei = new Rue("Sekoly Hei", Sekolintsika, Hei);
+        var heiBalancoire = new Rue("Hei Balancoire", Hei, Balancoire);
+        var pullmanNexta = new Rue("Pullman Nexta", Pullman, Nexta);
+        var balancoireEsti = new Rue("Balancoire Esti", Balancoire, Esti);
+        var balancoireBoulevard = new Rue("Balancoire Boulevar", Balancoire, Boulevard);
+        var boulevardEsti = new Rue("Boulevard Esti", Boulevard, Esti);
+
+        var lieux = List.of(Marais, Sekolintsika, Hei, Pullman, Nexta, Balancoire, Boulevard, Esti);
+        var Antananarivo = new Carte("Antananarivo", lieux);
+
+        var route = Bjarni.marche(Esti, Hei, Antananarivo).getMarche();
+        assertEquals(Hei, route.get(route.size() - 1));
+    }
 
 
 }
