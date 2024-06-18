561475e45d156d4e92a1b71334052d9c20488093
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-18 10:33:08 +0300

    refactor: refactor code

--- src/main/java/com/hei/Marche.java
+++ src/main/java/com/hei/Marche.java
@@ -2,7 +2,6 @@ package com.hei;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.util.List;
 
--- src/main/java/com/hei/carte/Carte.java
+++ src/main/java/com/hei/carte/Carte.java
@@ -1,18 +1,22 @@
-package com.hei;
+package com.hei.carte;
 
-import lombok.AllArgsConstructor;
+import com.hei.Lieu;
 import lombok.Getter;
 
 import java.util.List;
 
 
-@AllArgsConstructor
 @Getter
 public class Carte {
     private String nom;
     private List<Lieu> lieux;
 
+    public Carte() {
+    }
+
     public void ajouterLieux(Lieu lieu) {
         this.lieux.add(lieu);
     }
+
+
 }
