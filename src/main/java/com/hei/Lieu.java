0dc4f49b1b38601e6d68403a2a7817966e9b1f2e
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-18 10:33:30 +0300

    refactor: rm rueDisponible

--- src/main/java/com/hei/Lieu.java
+++ src/main/java/com/hei/Lieu.java
@@ -1,29 +1,35 @@
 package com.hei;
 
+import com.hei.carte.Carte;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
 public class Lieu {
     private final String nom;
     private final Set<Rue> rueInterconnect;
+    private final Carte carte;
+
+
+    public Lieu(String nom, Carte carte) {
+        this.nom = nom;
+        this.carte = carte;
+        this.rueInterconnect = new HashSet<>();
+        this.carte.ajouterLieux(this);
+    }
 
     public Lieu(String nom) {
         this.nom = nom;
         this.rueInterconnect = new HashSet<>();
+        this.carte = null;
+
     }
 
 
     public void ajouterRue(Rue rue) {
         this.rueInterconnect.add(rue);
-    }
-
-    public List<Rue> ruesDisponible() {
-        return new ArrayList<>(this.rueInterconnect);
     }
 
     @Override
